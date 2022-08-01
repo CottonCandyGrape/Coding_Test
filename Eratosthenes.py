e4de58b6873a66f6bf95e28e48f0880cfd3d9592
JeongHo16 <cup33c@naver.com>
2022-08-01 15:36:36 +0900

    추가 알고리즘 구현

--- Eratosthenes.py
+++ Eratosthenes.py
@@ -2,7 +2,7 @@ def eratosthenes(n):
     era = [True for _ in range(n+1)]
     era[0], era[1] = False, False
     primes = [] #prime 배열 안쓴다면 생략 가능
-    for i in range(2, n+1):
+    for i in range(2, int(n**0.5)+1): # 제곱근까지만 봐도 무방!
         if era[i]:
             primes.append(i) #prime 배열 안쓴다면 생략 가능
             for j in range(i*2, n+1, i):
