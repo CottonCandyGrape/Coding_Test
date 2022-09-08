8091ed03b2b145a4b06d8114eb2697e9a89badba
JeongHo16 <cup33c@naver.com>
2022-09-08 15:59:46 +0900

    Update Dijkstra.py

--- Dijkstra.py
+++ Dijkstra.py
@@ -10,25 +10,25 @@ distance = [INF]*(n+1)
 for _ in range(e):
     a, b, c = map(int, input().split()) #a->b로 가는 비용이 c
     graph[a].append((b,c))
-    #무방향 그래프라면 graph[b].append((a,c)) 추가로 해줘야 함
+    #graph[b].append((a,c)) #무방향 그래프라면 추가해야 함
 
 def dijkstra(start):
     q = []
-    distance[start] = 0
-    heapq.heappush(q, (0, start)) #우선순위 큐
+    distance[start] = 0 #시작 노드는 비용이 0
+    heapq.heappush(q, (distance[start], start)) #우선순위 큐
 
     while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist: continue 
-        #1)INF에서 갱신되니깐 방문했다면 보통 dist보다 작다.
-        #2) 현재(now)의 distance값보다 dist가 크다면 볼필요도 없다.
+        dist, now = heapq.heappop(q) #방문하지 않은 노드 중에서 최단 거리가 가장 짧은 노드 선택
+        if distance[now] < dist: #이미 방문 처리가 됐다고 간주할 수 있음.
+            continue 
+        
 
-        for i in graph[now]:
-            cost = dist + i[1] 
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0])) 
-                #distance를 초기화 or 갱신해야 어디가 더 빨리갈수있는지 알지!
+        for i in graph[now]:#현재(now)와 연결된 노드 중
+            cost = dist + i[1]
+            if cost < distance[i[0]]: #now를 거쳐 가는 비용이 더 작다면
+                distance[i[0]] = cost #갱신
+                heapq.heappush(q, (cost, i[0]))
+                #큐에 삽입(아직 방문 하지 않았기 때문. 방문했더라도 continue에서 걸림)
 
 dijkstra(s)
 
@@ -37,3 +37,19 @@ for i in range(1, n+1):
         print('Infinity')
     else:
         print(distance[i])
+
+'''
+input data
+6 11 1
+1 2 2
+1 3 5
+1 4 1
+2 3 3
+2 4 2
+3 2 3
+3 6 5
+4 3 3
+4 5 1
+5 3 1
+5 6 2
+'''
