43b7dda1155d2d9df038b8418ec8560d862beb47
JeongHo16 <cup33c@naver.com>
2022-09-13 18:13:32 +0900

    add comment

--- BFS.py
+++ BFS.py
@@ -11,6 +11,8 @@ def bfs(graph, start, visited):
                 queue.append(i)
                 visited[i] = True
 
+# 방문 기준 : 번호가 낮은 인접 노드 부터 방문한다고 가정.
+# 따라서 graph 각 행의 원소를 오름차순으로 초기화.
 graph = [
     [],
     [2, 3, 8],
--- DFS.py
+++ DFS.py
@@ -16,6 +16,8 @@ def dfs_stk(graph, v, visited): #stack으로 구현한 것인데... 좋아보이
             if not visited[graph[cv][i]]:
                 stk.append(graph[cv][i])
 
+# 방문 기준 : 번호가 낮은 인접 노드 부터 방문한다고 가정.
+# 따라서 graph 각 행의 원소를 오름차순으로 초기화.
 graph = [
     [],
     [2, 3, 8],
