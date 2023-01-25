45b02081cdf0c8805a853bc614eda11ec166b509
JeongHo16 <cup33c@naver.com>
2023-01-25 18:41:29 +0900

    a

--- Problems/boj_피보나치함수.py
+++ Problems/boj_피보나치함수.py
@@ -1,15 +1,14 @@
 #https://www.acmicpc.net/problem/1003
 #DP
 
-t = int(input())
+import sys
+input = sys.stdin.readline
 
+t = int(input())
+dp = [(1,0),(0,1)]
+for i in range(2, 41):
+    dp.append((dp[i-1][0]+dp[i-2][0], dp[i-1][1]+dp[i-2][1]))
+    
 for _ in range(t):
     n = int(input())
-    
-    dp = [(0,0)]*41
-    dp[0], dp[1] = (1,0), (0,1)
-
-    for i in range(2,n+1):
-        dp[i] = (dp[i-1][0]+dp[i-2][0], dp[i-1][1]+dp[i-2][1])
-
     print(*dp[n])
