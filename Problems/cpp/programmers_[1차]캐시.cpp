67d45ad864d5ee5fe8c553642a79b7c94b54095b
JeongHo16 <cup33c@naver.com>
2023-07-06 22:39:36 +0900

    Create programmers_[1차]캐시.cpp

---
+++ Problems/cpp/programmers_[1차]캐시.cpp
@@ -0,0 +1,93 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/17680
+//Implementation
+
+#include <string>
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <cctype>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities) {
+    const int ch=1, cm=5;
+    
+    if (cacheSize==0) return cities.size() * cm;
+    
+    int answer = 0;
+    list<string> slist;
+    unordered_map<string, bool> umap;
+    
+    for (string city : cities){
+        transform(city.begin(), city.end(), city.begin(), ::tolower);
+
+        //hit
+        if (umap[city]) {
+            for (auto it=slist.begin(); it!=slist.end(); it++){
+                if (*it == city){
+                    slist.erase(it);
+                    break;
+                }
+            }
+            
+            answer += ch;
+        }
+        //miss
+        else {
+            if (slist.size()>=cacheSize){
+                umap[slist.front()] = false;
+                slist.pop_front();
+            }
+            umap[city] = true;
+            
+            answer += cm;
+        }
+        
+        slist.push_back(city);
+    }
+
+    return answer;
+}
+
+
+/*
+[다른 사람 풀이]
+깔끔한데 cacheSize가 적어서 통과인 것 같다. vector에서 erase쓰는게 영 맘에 걸린다.
+cacheSize가 컷다면 내 풀이가 더 좋다.
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int solution(int cacheSize, vector<string> cities) {
+
+    vector <string> q;
+    int duration = 0;
+
+    for(vector <string>::iterator it = cities.begin(); it != cities.end(); it++){
+        transform(it->begin(), it->end(), it->begin(), ::tolower);
+
+        bool flag = false;
+        for(vector<string>::iterator itt = q.begin(); itt != q.end(); itt++){
+            if(*itt == *it) {
+                flag = true;
+                duration +=1;
+                q.erase(itt);
+                q.push_back(*it);
+                break;
+            }
+        }
+        if(!flag) {
+            duration +=5;
+            if(cacheSize != 0 && q.size() >= cacheSize)
+                q.erase(q.begin());
+            if(q.size() < cacheSize)
+                q.push_back(*it);
+        }
+    }
+
+    return duration;
+}
+*/
